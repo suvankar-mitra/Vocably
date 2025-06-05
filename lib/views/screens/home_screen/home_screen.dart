2f0b2f581b85718bc60899ff705fddcbedb69121
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2025-06-05 20:21:18 +0530

    Remove lottie animation from home, adding a score card

--- lib/views/screens/home_screen/home_screen.dart
+++ lib/views/screens/home_screen/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
 import 'package:vocably/constants/app_constants.dart';
 import 'package:vocably/themes/app_colors.dart';
 import 'package:vocably/widgets/home_search_bar_widget.dart';
+import 'package:vocably/widgets/score_board_widget.dart';
 import 'package:vocably/widgets/word_of_the_day_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,19 +14,14 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       body: Padding(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
+        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
         child: SingleChildScrollView(
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.all(30.0),
+                padding: const EdgeInsets.all(20.0),
                 child: Column(
                   children: [
-                    SizedBox(
-                      width: 100.0,
-                      height: 100.0,
-                      child: Lottie.asset('assets/lottie/lottie_floating_book_blue.json'),
-                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -48,11 +43,13 @@ class HomeScreen extends StatelessWidget {
               ),
 
               // Search bar
-              Padding(padding: const EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0), child: HomeSearchBarWidget()),
-              const SizedBox(height: 10.0),
+              Padding(padding: const EdgeInsets.all(10.0), child: HomeSearchBarWidget()),
+              // const SizedBox(height: 10.0),
               // Word of the day widget
-              Padding(padding: const EdgeInsets.only(top: 20.0, left: 10.0, right: 10.0), child: WordOfTheDayWidget()),
-              const SizedBox(height: 30.0),
+              Padding(padding: const EdgeInsets.all(10.0), child: WordOfTheDayWidget()),
+
+              // const SizedBox(height: 10.0),
+              Padding(padding: const EdgeInsets.all(10.0), child: ScoreBoardWidget()),
             ],
           ),
         ),
