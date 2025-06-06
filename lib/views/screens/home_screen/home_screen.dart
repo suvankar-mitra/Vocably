f520c4e6d4714eb2340f8158307884a81b4426d3
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2025-06-07 01:37:00 +0530

    Moving the title to AppBar

--- lib/views/screens/home_screen/home_screen.dart
+++ lib/views/screens/home_screen/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:vocably/constants/app_constants.dart';
 import 'package:vocably/themes/app_colors.dart';
 import 'package:vocably/widgets/home_search_bar_widget.dart';
 import 'package:vocably/widgets/score_board_widget.dart';
+import 'package:vocably/widgets/search_history_card_widget.dart';
 import 'package:vocably/widgets/word_of_the_day_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,45 +14,46 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            text: AppConstants.appName,
-                            style: GoogleFonts.playfairDisplay(
-                              fontSize: Theme.of(context).textTheme.displayMedium?.fontSize,
-                              fontWeight: FontWeight.bold,
-                              color: AppColors.appTitleColor,
-                            ),
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RichText(
+            text: TextSpan(
+              text: AppConstants.appName,
+              style: GoogleFonts.playfairDisplay(
+                fontSize: Theme.of(context).textTheme.displayMedium?.fontSize,
+                fontWeight: FontWeight.bold,
+                color: AppColors.activeNavIconColor,
               ),
-
-              // Search bar
-              Padding(padding: const EdgeInsets.all(10.0), child: HomeSearchBarWidget()),
-              // const SizedBox(height: 10.0),
-              // Word of the day widget
-              Padding(padding: const EdgeInsets.all(10.0), child: WordOfTheDayWidget()),
-
-              // const SizedBox(height: 10.0),
-              Padding(padding: const EdgeInsets.all(10.0), child: ScoreBoardWidget()),
-            ],
+            ),
+            textAlign: TextAlign.left,
           ),
+        ),
+        centerTitle: true,
+        backgroundColor: AppColors.bottomNavBackground,
+        elevation: 2,
+        bottom: PreferredSize(preferredSize: const Size.fromHeight(8.0), child: Container()),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Search bar
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, left: 10.0, right: 10.0, bottom: 10.0),
+              child: HomeSearchBarWidget(),
+            ),
+
+            // Word of the day widget
+            Padding(padding: const EdgeInsets.all(10.0), child: WordOfTheDayWidget()),
+
+            // Search history card widget
+            Padding(padding: const EdgeInsets.all(10.0), child: SearchHistoryCardWidget()),
+
+            // Score board widget
+            Padding(padding: const EdgeInsets.all(10.0), child: ScoreBoardWidget()),
+
+            const SizedBox(height: 20.0),
+          ],
         ),
       ),
     );
