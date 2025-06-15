076e0d299f0921e3374c4e7ca11df55cd1a183fd
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2025-06-15 21:47:47 +0530

    Remove unused variable `isDark`
    The `isDark` variable, which checked the theme's brightness, was not utilized in the `HomeSearchBarWidget`. This commit removes the unused variable.

--- lib/widgets/home_search_bar_widget.dart
+++ lib/widgets/home_search_bar_widget.dart
@@ -10,7 +10,6 @@ class HomeSearchBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final isDark = theme.brightness == Brightness.dark;
 
     return InkWell(
       onTap: () {
