feff2291676e757d5ec5fece1cbf8f963cc554c9
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2025-06-15 21:09:00 +0530

    Refactor: Simplify theme access in LandingPage
    Refactored the `LandingPage` widget to access theme properties more concisely by storing `Theme.of(context)` in a local variable.
    
    Additionally, the navigation bar height was explicitly set to `64.0`.

--- lib/views/screens/landing_page.dart
+++ lib/views/screens/landing_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
-import 'package:vocably/themes/app_colors.dart';
 import 'package:vocably/views/screens/bookmark_page.dart';
 import 'package:vocably/views/screens/home_screen/home_screen.dart';
 import 'package:vocably/views/screens/settings_page.dart';
@@ -25,45 +24,39 @@ class _LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      backgroundColor: theme.scaffoldBackgroundColor,
       body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
       bottomNavigationBar: NavigationBar(
         selectedIndex: _selectedIndex,
         onDestinationSelected: _onItemTapped,
-        backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+        backgroundColor: theme.bottomNavigationBarTheme.backgroundColor,
         indicatorColor: Colors.transparent,
+        height: 64.0,
         destinations: <NavigationDestination>[
           NavigationDestination(
-            icon: Icon(
-              HugeIcons.strokeRoundedHome01,
-              color: Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
-            ),
-            selectedIcon: Icon(
-              HugeIcons.strokeRoundedHome01,
-              color: Theme.of(context).bottomNavigationBarTheme.selectedItemColor,
-            ),
+            icon: Icon(HugeIcons.strokeRoundedHome01, color: theme.bottomNavigationBarTheme.unselectedItemColor),
+            selectedIcon: Icon(HugeIcons.strokeRoundedHome01, color: theme.bottomNavigationBarTheme.selectedItemColor),
             label: '',
           ),
           NavigationDestination(
             icon: Icon(
               HugeIcons.strokeRoundedBookBookmark01,
-              color: Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
+              color: theme.bottomNavigationBarTheme.unselectedItemColor,
             ),
             selectedIcon: Icon(
               HugeIcons.strokeRoundedBookBookmark01,
-              color: Theme.of(context).bottomNavigationBarTheme.selectedItemColor,
+              color: theme.bottomNavigationBarTheme.selectedItemColor,
             ),
             label: '',
           ),
           NavigationDestination(
-            icon: Icon(
-              HugeIcons.strokeRoundedSettings01,
-              color: Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
-            ),
+            icon: Icon(HugeIcons.strokeRoundedSettings01, color: theme.bottomNavigationBarTheme.unselectedItemColor),
             selectedIcon: Icon(
               HugeIcons.strokeRoundedSettings01,
-              color: Theme.of(context).bottomNavigationBarTheme.selectedItemColor,
+              color: theme.bottomNavigationBarTheme.selectedItemColor,
             ),
             label: '',
           ),
